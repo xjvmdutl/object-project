c55ae69ac7e79b83a98b5c355fe4bf97a93ccec9
junhokim <junhokim@gmarket.com>
2023-01-30 01:29:23 +0900

    Initial commit

---
+++ src/main/java/com/gmarket/objectproject/ObjectProjectApplication.java
@@ -0,0 +1,13 @@
+package com.gmarket.objectproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObjectProjectApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ObjectProjectApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/gmarket/objectproject/ObjectProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmarket.objectproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObjectProjectApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
