269e86ea2b05d8a73c1755d958587276b491472d
junhokim <junhokim@gmarket.com>
2023-01-31 18:01:17 +0900

    설계 개선하기

--- src/main/java/com/gmarket/objectproject/Bag.java
+++ src/main/java/com/gmarket/objectproject/Bag.java
@@ -14,7 +14,7 @@ public class Bag {
     this.amount = amount;
   }
 
-  public boolean hasInvitation(){
+  private boolean hasInvitation(){
     return invitation != null;
   }
 
@@ -22,11 +22,12 @@ public class Bag {
     return ticket != null;
   }
 
-  public void setTicket(Ticket ticket){
+  //내부에서만 사용하기 때문에 private로 변경
+  private void setTicket(Ticket ticket){
     this.ticket = ticket;
   }
 
-  public void minusAmount(Long amount){
+  private void minusAmount(Long amount){
     this.amount -= amount;
   }
 
@@ -34,4 +35,14 @@ public class Bag {
     this.amount += amount;
   }
 
+  public Long hold(Ticket ticket){
+    if(hasInvitation()){
+      setTicket(ticket);
+      return 0L;
+    }else{
+      setTicket(ticket);
+      minusAmount(ticket.getFee());
+      return ticket.getFee();
+    }
+  }
 }
