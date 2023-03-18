58ffe818cbece06f3d8c90c239f433fbdeb1dfd7
junhokim <junhokim@gmarket.com>
2023-03-18 14:23:21 +0900

    계약에 의한 설계
    - 사전조건
    - 사후조건
    - 불변식

--- src/main/java/com/gmarket/objectproject/invitation/Bag.java
+++ src/main/java/com/gmarket/objectproject/invitation/Bag.java
@@ -14,6 +14,12 @@ public class Bag {
     this.amount = amount;
   }
   public Long hold(Ticket ticket){
+    /**
+     * 사후조건(하나 이상의 리턴을 가질 경우)
+     * Contract.Result<T>는 메서드 실행이 끝난 후에 실제로 반환되는 값을 전달하기 때문에 몇 번의 return문이 나오더라도 한번만 기술하면 된다.
+     */
+    //Contract.Ensures(Contract.Result<Long>() >= 0)
+
     if (hasInvitation()) {
       this.ticket = ticket;
       return 0L;
