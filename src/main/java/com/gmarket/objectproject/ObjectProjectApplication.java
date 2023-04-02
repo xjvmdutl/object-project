b69aed2fb72481bddc5e613d2289ea51ae36fcd0
junhokim <junhokim@gmarket.com>
2023-04-03 01:40:08 +0900

    test

--- src/main/java/com/gmarket/objectproject/ObjectProjectApplication.java
+++ src/main/java/com/gmarket/objectproject/ObjectProjectApplication.java
@@ -3,6 +3,9 @@ package com.gmarket.objectproject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * TEST
+ */
 @SpringBootApplication
 public class ObjectProjectApplication {
   public static void main(String[] args) {
