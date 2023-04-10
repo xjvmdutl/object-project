b53d4454575ebb025c24ba80c2d5f613f77d03af
junhokim <junhokim@gmarket.com>
2023-04-10 10:54:43 +0900

    test5

--- src/main/java/com/gmarket/objectproject/ObjectProjectApplication.java
+++ src/main/java/com/gmarket/objectproject/ObjectProjectApplication.java
@@ -4,8 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * TEST5
- *
+ * TEST7
  */
 @SpringBootApplication
 public class ObjectProjectApplication {
