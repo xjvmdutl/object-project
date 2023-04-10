4f8d5cafd44f45af40128476e5d9c0abc658e707
junhokim <junhokim@gmarket.com>
2023-04-10 10:56:35 +0900

    test8

--- src/main/java/com/gmarket/objectproject/ObjectProjectApplication.java
+++ src/main/java/com/gmarket/objectproject/ObjectProjectApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * TEST7
+ * TEST8
  */
 @SpringBootApplication
 public class ObjectProjectApplication {
