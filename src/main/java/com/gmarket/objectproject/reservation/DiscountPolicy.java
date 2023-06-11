d01a1028b3a5b9e16ff9c276d61edcd8d2b8ac5b
junhokim <junhokim@gmarket.com>
2023-06-11 21:52:30 +0900

    계약에 의한 설계와 서브타이핑

--- src/main/java/com/gmarket/objectproject/reservation/DiscountPolicy.java
+++ src/main/java/com/gmarket/objectproject/reservation/DiscountPolicy.java
@@ -1,24 +1,43 @@
 package com.gmarket.objectproject.reservation;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public abstract class DiscountPolicy {
+
   private List<DiscountCondition> conditions = new ArrayList<>();
 
-  public DiscountPolicy(DiscountCondition ... conditions) {
+  public DiscountPolicy(DiscountCondition... conditions) {
     this.conditions = Arrays.asList(conditions);
   }
 
+  /**
+   * 암묵적인 계약 조건이 있었다.
+   */
   public Money calculateDiscountAmount(Screening screening) {
-    for(DiscountCondition each : conditions) {
+    checkPrecondition(screening); //사전 조건 체크
+
+    Money amount = Money.ZERO;
+    for (DiscountCondition each : conditions) {
       if (each.isSatisfiedBy(screening)) {
-        return getDiscountAmount(screening);
+        amount = getDiscountAmount(screening);
+        checkPostcondition(amount); //사후 조건 체크
+        return amount;
       }
     }
+    amount = screening.getMovieFee();
+    checkPostcondition(amount); //사후 조건 체크
+    return amount;
+  }
 
-    return screening.getMovieFee();
+  protected void checkPrecondition(Screening screening) {
+    assert screening != null && screening.getStartTime().isAfter(LocalDateTime.now());
+  }
+
+  protected void checkPostcondition(Money amount) {
+    assert amount != null && amount.isGreaterThanOrEqual(Money.ZERO);
   }
 
   abstract protected Money getDiscountAmount(Screening Screening);
---
+++ src/main/java/com/gmarket/objectproject/reservation/exception/InvalidScreeningException.java
@@ -0,0 +1,5 @@
+package com.gmarket.objectproject.reservation.exception;
+
+public class InvalidScreeningException extends RuntimeException{
+
+}
