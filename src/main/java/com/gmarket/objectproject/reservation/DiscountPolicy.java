5b8f78936158b44ee998d0b59e9bb6cfe5d8967f
junhokim <junhokim@gmarket.com>
2023-06-18 13:38:05 +0900

    설계에 일관성 부여하기

--- src/main/java/com/gmarket/objectproject/reservation/DiscountPolicy.java
+++ src/main/java/com/gmarket/objectproject/reservation/DiscountPolicy.java
@@ -13,32 +13,13 @@ public abstract class DiscountPolicy {
     this.conditions = Arrays.asList(conditions);
   }
 
-  /**
-   * 암묵적인 계약 조건이 있었다.
-   */
   public Money calculateDiscountAmount(Screening screening) {
-    checkPrecondition(screening); //사전 조건 체크
-
-    Money amount = Money.ZERO;
     for (DiscountCondition each : conditions) {
       if (each.isSatisfiedBy(screening)) {
-        amount = getDiscountAmount(screening);
-        checkPostcondition(amount); //사후 조건 체크
-        return amount;
+        return getDiscountAmount(screening);
       }
     }
-    amount = screening.getMovieFee();
-    checkPostcondition(amount); //사후 조건 체크
-    return amount;
+    return screening.getMovieFee();
   }
-
-  protected void checkPrecondition(Screening screening) {
-    assert screening != null && screening.getStartTime().isAfter(LocalDateTime.now());
-  }
-
-  protected void checkPostcondition(Money amount) {
-    assert amount != null && amount.isGreaterThanOrEqual(Money.ZERO);
-  }
-
   abstract protected Money getDiscountAmount(Screening Screening);
 }
